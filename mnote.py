4e39a1510f8f4a57795b9a6a276742dbffc12204
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2023-11-21 17:38:52 -0500

    feat: Parse command-line arguments with argparse

--- mnote.py
+++ mnote.py
@@ -1,7 +1,22 @@
+import argparse
 import tkinter as tk
 
 
+def parse_args() -> argparse.Namespace:
+    """
+    Parses command-line arguments.
+
+    Returns:
+        argparse.Namespace: An object containing the parsed arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    return parser.parse_args()
+
+
 def main():
+    # args: argparse.Namespace = parse_args()
+
     root = tk.Tk()
     root.title("mnote")
     root.mainloop()
