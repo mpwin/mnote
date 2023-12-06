cdac577d5cd2f82e28ff2edcbf7db7546131a469
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2023-12-06 17:56:24 -0500

    feat: Update batch file to use mnote_picker.py

--- mnote.bat
+++ mnote.bat
@@ -1,20 +1,6 @@
 @echo off
 
-setlocal EnableDelayedExpansion
-set "mnotesDir=%USERPROFILE%\Mnotes"
-set /a count=0
+set "mnotes=%USERPROFILE%\Mnotes"
+for /f "delims=" %%a in ('py mnote_picker.py %mnotes%') do set "mnote=%%a"
 
-for /d %%D in ("%mnotesDir%\*.mnote") do (
-    set /a count+=1
-    set "dir[!count!]=%%D"
-)
-
-if %count% == 0 (
-    echo No .mnotes found in %mnotesDir%.
-    exit /b
-)
-
-set /a index=(%random% %% %count%) + 1
-set "mnote=!dir[%index%]!"
-
-py %~dp0mnote.py %mnote%
+py %~dp0mnote_reader.py "%mnote%"
