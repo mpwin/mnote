0dccec00e31ce7d601b7c7c194332f6e2bc4efdf
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2023-12-07 00:36:40 -0500

    fix: Missing mnote_picker.py error

--- mnote.bat
+++ mnote.bat
@@ -1,6 +1,6 @@
 @echo off
 
 set "mnotes=%USERPROFILE%\Mnotes"
-for /f "delims=" %%a in ('py mnote_picker.py %mnotes%') do set "mnote=%%a"
+for /f "delims=" %%a in ('py %~dp0mnote_picker.py %mnotes%') do set "mnote=%%a"
 
 py %~dp0mnote_reader.py "%mnote%"
