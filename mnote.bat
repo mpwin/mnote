dd3a919bc0776fc794f38b53ecab8f15e0a385c5
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2024-03-20 16:32:55 -0400

    fix: Invalid argument bug when filtering mnotes

--- mnote.bat
+++ mnote.bat
@@ -1,9 +1,11 @@
 @echo off
 
-set "mpath=%1"
+if "%~1"=="" (
+    set "mpath=%USERPROFILE%\Mnotes"
+) else (
+    set "mpath=%~1"
+)
 
-if "%mpath%"=="" set "mpath=%USERPROFILE%\Mnotes"
-
-for /f "delims=" %%a in ('py %~dp0mnote_picker.py %mpath%') do set "mnote=%%a"
+for /f "delims=" %%a in ('py %~dp0mnote_picker.py "%mpath%"') do set "mnote=%%a"
 
 py %~dp0mnote_reader.py "%mnote%"
