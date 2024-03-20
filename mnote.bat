9b2f4cecfa9c06920f0a9f23dbe5aab63a672ab9
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2024-03-20 16:56:22 -0400

    refactor: Clean up mnote_picker.py call

--- mnote.bat
+++ mnote.bat
@@ -9,6 +9,10 @@ if "%~1"=="" (
 :: Remove trailing backslash if present
 if "%mpath:~-1%"=="\" set "mpath=%mpath:~0,-1%"
 
-for /f "delims=" %%a in ('py %~dp0mnote_picker.py "%mpath%"') do set "mnote=%%a"
+for /f "delims=" %%a in ('
+    py %~dp0mnote_picker.py "%mpath%" ^
+') do (
+    set "mnote=%%a"
+)
 
 py %~dp0mnote_reader.py "%mnote%"
