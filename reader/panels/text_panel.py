b911601a8e7039e5944a7c52f4ae873f52ef7bf0
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2024-04-03 18:08:16 -0400

    feat: Dynamically calculate text width and height

--- reader/panels/text_panel.py
+++ reader/panels/text_panel.py
@@ -17,18 +17,28 @@ class TextPanel(BasePanel):
 
         text: tk.Text = tk.Text(self)
         text.insert(tk.END, data['text'])
-        text.config(**self.config())
+        text.config(**self.config(data))
 
         text.pack()
         self.pack(padx=40, pady=40, side='left')
 
-    def config(self) -> dict:
+    def config(self, data: dict) -> dict:
         """Configures the text widget used within the TextPanel."""
+
+        def height(text: str) -> int:
+            return len(text.split('\n'))
+
+        def width(text: str) -> int:
+            line_lengths = [len(line) for line in text.split('\n')]
+            return max(line_lengths)
+
         return {
             'bd': 0,
             'bg': '#1e1e1e',
             'fg': '#d4d4d4',
             'font': ('Consolas', 16),
+            'height': height(data['text']),
             'state': 'disabled',
+            'width': width(data['text']),
             'wrap': 'word',
             }
