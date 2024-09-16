343314744c099e98aea856ef10bf21b024938428
Mateusz Winiarczyk <matthew.winiarczyk@gmail.com>
2024-09-16 15:49:01 +0200

    refactor: Clean up RecallSampleWidget

--- reader/widgets/recall_sample_widget.py
+++ reader/widgets/recall_sample_widget.py
@@ -16,27 +16,44 @@ class RecallSampleWidget(BaseWidget):
         """
         super().__init__(parent, data)
 
-        sample_size: int = min(10, len(data.get('items', [])))
-        sample: list = random.sample(data.get('items', []), sample_size)
+        sample: list = random.sample(self.data['items'], self.sample_size)
+        self.display_items(sample)
 
-        for item in sample:
+        self.config(self.frame_config)
+        self.pack(self.pack_config)
+
+    @property
+    def frame_config(self) -> dict:
+        return {
+            'bg': '#1e1e1e',
+            'padx': 40,
+            'pady': 40,
+            }
+
+    @property
+    def label_config(self) -> dict:
+        return {
+            'bg': '#1e1e1e',
+            'fg': '#d4d4d4',
+            'font': ('Consolas', 16),
+            'text': "?",
+            'width': 40,
+            }
+
+    @property
+    def sample_size(self) -> int:
+        return min(self.data.get('sample_size', 10), len(self.data['items']))
+
+    def display_items(self, items: list) -> None:
+        for item in items:
             item_frame: tk.Frame = tk.Frame(self)
             item_frame.pack()
 
             for column, text in enumerate(item):
-                label: tk.Label = tk.Label(
-                    item_frame,
-                    bg='#1e1e1e',
-                    fg='#d4d4d4',
-                    font=('Consolas', 16),
-                    text='[···]',
-                    width=20,
-                    )
+                label: tk.Label = tk.Label(item_frame, **self.label_config)
                 label.grid(row=0, column=column)
 
                 label.bind(
                     '<Button-1>',
                     lambda e, label=label, text=text: label.config(text=text),
                     )
-
-        self.pack(self.pack_config)
