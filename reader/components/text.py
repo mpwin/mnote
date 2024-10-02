95e3224d9a56307c71d4ea8380084ac8803df518
Mateusz Winiarczyk <matthew.winiarczyk@gmail.com>
2024-10-02 16:06:13 +0200

    refactor: Add Text component for wrapping Tkinter Text widgets

---
+++ reader/components/text.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+
+
+class Text():
+    """A wrapper for the Tkinter Text widget."""
+
+    default_config: dict = {
+        'bd': 0,
+        'state': 'disabled',
+        'wrap': 'word',
+        }
+
+    def __init__(self, text: str, config: dict = {}) -> None:
+        """Initializes a new Text component instance."""
+        self._text: str = text
+        self._config: dict = {**self.default_config, **config}
+
+    def __len__(self) -> int:
+        return len(self._text)
+
+    def __getitem__(self, index: int) -> str:
+        return self._text[index]
+
+    @property
+    def config(self) -> dict:
+        return self._config
+
+    @config.setter
+    def config(self, config: dict) -> None:
+        self._config.update(config)
+
+    @property
+    def text(self) -> str:
+        return self._text
+
+    def pack(self, parent: tk.Widget) -> None:
+        self._widget: tk.Text = tk.Text(parent)
+        self._widget.insert(tk.END, self.text)
+        self._widget.config(self.config)
+        self._widget.pack()
